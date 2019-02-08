b6ebf4bf4e0511bfb3522704aa6da652fbc1dd69
Khuong Chu <joe@x7.fi>
2019-02-08 11:12:36 +0200

    Create idol to firebase with hardcode

--- src/IdolList.js
+++ src/IdolList.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { connect } from "./store";
-import { FETCH_REGION } from "./actions";
+import {connect} from "./store";
+import {FETCH_REGION} from "./actions";
 import firebase from "./Firebase";
 import IdolListItem from "./IdolListItem";
 import IdolModalForm from "./IdolModalForm";
-import { Button } from "react-bootstrap";
-
+import {Button} from "react-bootstrap";
 
 const mapStateToProps = (state, props) => ({
   ...props
@@ -13,7 +12,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
   setRegion: regions => {
-    dispatch({ type: FETCH_REGION, payload: regions });
+    dispatch({type: FETCH_REGION, payload: regions});
   }
 });
 
@@ -25,41 +24,58 @@ class IdolList extends React.Component {
   }
 
   handleClose = () => {
-    this.setState({ show: false });
+    this.setState({show_modal: false, modal_idol: null});
   };
 
   handleShow = () => {
-    this.setState({ show: true });
+    this.setState({show_modal: true});
   };
 
-  handleItemEdit = (item) =>  {
-    this.setState({ modal_idol: item }, () => (this.handleShow()));
+  handleItemEdit = (item) => {
+    this.setState({
+      modal_idol: item
+    }, () => (this.handleShow()));
+  }
+
+  saveIdol = () => {
+    firebase.firestore().collection('idols').add({firstname: "Phong", lastname: "Ly"}).then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    }).catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
   }
 
   render() {
-    return (
-      <React.Fragment>
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(this.props.idols).map((item, index) => {
+    return (<React.Fragment>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Object.keys(this.props.idols).map((item, index) => {
               return <IdolListItem idol={this.props.idols[item]} key={item} onItemClick={this.handleItemEdit}/>;
-            })}
-          </tbody>
-        </table>
+            })
+          }
+        </tbody>
+      </table>
 
-        <IdolModalForm show={this.state.show} idol={this.state.modal_idol} onClose={this.handleClose}/>
-      </React.Fragment>
-    );
+      <Button variant="primary" onClick={this.saveIdol}>
+        Create New
+      </Button>
+
+      {
+        (() => {
+          if (this.state.show_modal) {
+            return (<IdolModalForm show={this.state.show_modal} idol={this.state.modal_idol} onClose={this.handleClose}/>);
+          }
+        })()
+      }
+    </React.Fragment>);
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(IdolList);
+export default connect(mapStateToProps, mapDispatchToProps)(IdolList);
--- src/IdolModalForm.js
+++ src/IdolModalForm.js
@@ -1,27 +1,61 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import {Modal, Button, Form, Image} from "react-bootstrap";
 
 class IdolModalForm extends React.Component {
 
+  constructor(props) {
+    console.log(props);
+    super(props);
+    this.state = {
+      idol: props.idol
+    };
+  }
+
+  handleOnChange = e => {
+   e.preventDefault(e);
+   this.setState({
+     [e.target.name]: e.target.value
+   }, () => (console.log(this.state)));
+ };
+
   render() {
-    return (
-      <React.Fragment>
-        <Modal show={this.props.show} onHide={this.props.onClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>{this.props.idol == null ? 'Create new idol' : `Edit ${this.props.idol.first} ${this.props.idol.last}`}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.props.onClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={this.props.onClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </React.Fragment>
-    );
+    console.log(this.state.idol);
+    return (<React.Fragment>
+      <Modal show={this.props.show} onHide={this.props.onClose}>
+        <Modal.Header closeButton="closeButton">
+          <Modal.Title>{
+              this.props.idol == null
+                ? 'Create new idol'
+                : `Edit ${this.props.idol.first} ${this.props.idol.last}`
+            }</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Firstname</Form.Label>
+              <Form.Control type="text" name="first" placeholder="Firstname" value={this.state.idol != null ? this.state.idol.first : ""} onChange={this.handleOnChange}/>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Lastname</Form.Label>
+              <Form.Control type="text" name="last" placeholder="Lastname" value={this.state.idol != null ? this.state.idol.last : ""} onChange={this.handleOnChange}/>
+            </Form.Group>
+            <Form.Group controlId="formBasicChecbox">
+              <Form.Label>Image URL</Form.Label>
+              <Form.Control type="text" placeholder="Image Url" onChange={this.handleOnChange}/>
+              <Image className="mt-3" src="https://via.placeholder.com/150/d32776" rounded />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.props.onClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={this.props.onClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </React.Fragment>)
   }
 }
 
